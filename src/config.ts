7541100164b7351e976d80bd9ad6b87fd429ac99
Issam SISBANE <issam.sisbane@etu.univ-lyon1.fr>
2024-02-08 23:41:32 +0100

    add template & start to adapt it

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Issam SISBANE | PORTFOLIO';
+export const SITE_DESCRIPTION = 'This website is the portolio of Mr.Issam SISBANE, a cloud and backend engineer.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
