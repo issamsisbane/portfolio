cee9eca8569ddff87b47a466ba31cce20f324090
Issam SISBANE <issam.sisbane@thuasne.fr>
2024-04-24 11:46:01 -0500

    add partytown and update google analytics tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://IssamSisbane.github.io',
   base: '/portfolio',
-  integrations: [mdx(), sitemap(), tailwind(), vue()],
+  integrations: [mdx(), sitemap(), tailwind(), vue(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),],
   i18n: {
     defaultLocale: "fr",
     locales: ["en", "fr"],
