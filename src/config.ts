ad835c04448d9ea6ebb2205839f256f3b4dd26dd
Issam SISBANE <issam.sisbane@atos.net>
2025-12-26 14:55:21 +0000

    fix: correct SITE DESCRIPTION

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '';
-export const SITE_DESCRIPTION = 'This website is the portolio of Issam SISBANE, a cloud and backend engineer.';
+export const SITE_DESCRIPTION = 'This website is the portolio of Issam SISBANE, a Cloud and DevOps Engineer.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
